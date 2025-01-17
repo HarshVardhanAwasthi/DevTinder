f30de42876210ee32697e4c449b3a55961784815
Harsh V Awasthi <evearywherefor4@gmail.com>
2025-01-17 17:00:53 +0530

    sequence of a route handlers matters a lot in express js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const express=require("express")
 
 const app=express();
+
+// Routes are Matched Sequentially
+
+// Express checks routes from top to bottom; the first match is executed.
+
+// Specific Routes Before General Routes
+
+//  Place specific routes (e.g., /users) before general routes (e.g., /).
+
 app.use("/test", (req,res)=>{
     res.send("hello to testing phase!!")
 })
@@ -10,7 +19,13 @@ app.use("/db", (req,res)=>{
 })
 app.use("/start",(req,res)=>{
     res.send("hello to starting the kaam!")
+
 })
+
+app.use("/",(req, res)=>{
+    res.send("hahahahahha mai aa gaya!!")
+})
+
 app.listen(7000,()=>{
     console.log("welcome to port no 7000!!")
 })
