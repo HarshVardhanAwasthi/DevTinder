606a28379acb378bd07e239faad0e93ab459f6d1
Harsh V Awasthi <evearywherefor4@gmail.com>
2025-01-17 18:36:41 +0530

    Exploring Route Parameters

--- src/app.js
+++ src/app.js
@@ -14,16 +14,24 @@ app.post("/user",(req, res)=>{
     res.send("data saved on server");
 });
 
+//Route parameters are named URL segments that are used to capture the values specified at their position in the URL. The captured values are populated in the req.params object, with the name of the route parameter specified in the path as their respective keys.
+
+// USERID AND BOOK PARAMS 
+
 app.get('/users/:userId/books/:bookId', (req, res) => {
     res.send(req.params)
   })
 
-  app.get('/flights/:from-:to', (req, res) => {
+//  FROM TO PARAMS
+
+app.get('/flights/:from-:to', (req, res) => {
     console.log(req.params);
     res.send(req.params);
   })
 
-  app.get('/username/:username/password/:passkey', (req, res) => {
+// USERNAME AND PASSWORD PARAMS
+
+app.get('/username/:username/password/:passkey', (req, res) => {
     console.log(req.params);
     res.send(req.params);
   })
