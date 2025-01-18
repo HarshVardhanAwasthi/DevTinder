a0d76e1e98ac3b69595233cd8ac4abbc2f1423ec
Harsh V Awasthi <evearywherefor4@gmail.com>
2025-01-18 17:42:59 +0530

    exploring multiple request handlers

--- src/app.js
+++ src/app.js
@@ -2,42 +2,29 @@ const express=require("express")
 
 const app=express();
 
-app.get("/user",(req, res)=>{
-    res.send({
-        "first name":"Harss",
-        "last name":"Awassthi"
-    })
-});
 
-app.post("/user",(req, res)=>{
-    console.log("save data to database");
-    res.send("data saved on server");
-});
+// Multiple handlers run sequentially.
+// Use next() to move to the next handler.
 
-//Route parameters are named URL segments that are used to capture the values specified at their position in the URL. The captured values are populated in the req.params object, with the name of the route parameter specified in the path as their respective keys.
+//THE CODE BELOW GIVES ERROR BECAUSE ONCE YOU HAVE SEND RESPONSE TO THE REQUEST, THE CONNECTION BREAKS AND WHEN 2ND TIME YOU SEND RESPONSE TO THE  REQUEST IT GIVES ERROR BECAUSE IT DID NOT FIND ANY CLIENT !!!!!!
 
-// USERID AND BOOK PARAMS 
+//YAHAN PE YE HO RHA HAI KI TUMNE APP.USE KIA REQUEST SERVER KE PAAS GAI USNE SEND KIA RESPONSE ("res.send("sdfkjak")") PHIR CONNECTION BREAK HO GAYA USKE BAAD JAB NEXT RUN HUA TO USNE DOOSRE REQUEST HANDLER KO BHEJ DIA LEKIN JAB WO RESPONSE SEND KREGA TO ERROR AAEGI KYUNKI CLIENT HAI NHI CONNECTION PAHLE RESPONSE KE BAAD HI KHATAM HO GAYA THA
 
-app.get('/users/:userId/books/:bookId', (req, res) => {
-    res.send(req.params)
-  })
+app.use(
+    "/hello",
+    (req,res,next)=>{
+        console.log("1st route handler")
+        res.send("1st response!!");
+        next();
+        
+    },
 
-//  FROM TO PARAMS
+    (req,res)=>{
+        console.log("2nd route handler")
+        res.send("2nd resoponse");
+    }
 
-app.get('/flights/:from-:to', (req, res) => {
-    console.log(req.params);
-    res.send(req.params);
-  })
-
-// USERNAME AND PASSWORD PARAMS
-
-app.get('/username/:username/password/:passkey', (req, res) => {
-    console.log(req.params);
-    res.send(req.params);
-  })
-// app.use("/",(req, res)=>{
-//     res.send("hahahahahha mai aa gaya!!")
-// })
+)
 
 app.listen(7000,()=>{
     console.log("welcome to port no 7000!!")
