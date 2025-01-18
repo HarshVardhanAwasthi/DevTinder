4b6b33aa4f893dbd5b22ab940bea5780d97c58af
Harsh V Awasthi <evearywherefor4@gmail.com>
2025-01-18 19:32:56 +0530

    Cannot GET /hello

--- src/app.js
+++ src/app.js
@@ -2,22 +2,13 @@ const express=require("express")
 
 const app=express();
 
-/*
- No Response is Sent:
 
-    -The server receives the request but doesn't send a response (res.send(), res.json(), or res.end() is missing).
-    -The client (browser, Postman, or any HTTP client) will keep waiting until the request times out.
-
- Request Hangs Until Timeout:
-
-    -Since no response is sent, the client will keep waiting indefinitely.
-    -Eventually, the browser or client may timeout*/
 
 app.use("/users",(req,res)=>{
 
 })
 
-
+// lagatar next next use krne ki wajah se error aa rhi hai 
 
 app.use(
     "/hello",
@@ -31,16 +22,10 @@ app.use(
     (req,res,next)=>{
         console.log("2nd route handler")
         // res.send("1st response!!");
-        // next();
+        next();
         
     },
 )
-/*
-    2nd  middleware kisi response ko bhej nahi raha (res.send()), aur next() bhi call nahi ho raha.
-
-    -Is wajah se request hang ho jayegi, aur client (browser/Postman) wait karta rahega.
-    -Browser me loading icon ghoomta rahega aur request complete nahi hogi.
-*/
 
 app.listen(7000,()=>{
     console.log("welcome to port no 7000!!")
