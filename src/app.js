6c4762a547c66f743fc35ca6104dc85c59ca8efb
Harsh V Awasthi <evearywherefor4@gmail.com>
2025-01-19 10:51:11 +0530

    middleware and route handler (exploring real use case)

--- src/app.js
+++ src/app.js
@@ -2,30 +2,33 @@ const express=require("express")
 
 const app=express();
 
+//this is middleware which is used for authentication purpose.
+//here app.use is used because it will handle all calls of "/admin" call weather it is get post delete etc.
+//each call will first go through this middleware for authentication and then go to final route handler. 
 
-
-app.use("/users",(req,res)=>{
-
+app.use("/admin",(req,res,next)=>{
+    const token="xyz";
+    const isAdminAuthorized = token == "xxyz";
+    if(!isAdminAuthorized){
+        res.status(401).send("not authorized user");
+    }
+    else{
+        next();
+    }
 })
 
-// lagatar next next use krne ki wajah se error aa rhi hai 
+//these app.get and app.delte are route handleres which will get executed once the admin is authorized and send the response back to client.
+//app.get, app.post, app.delte, etc can be route handlers depending on the purpose of route handler.
 
-app.use(
-    "/hello",
-    (req,res,next)=>{
-        console.log("1st route handler")
-        // res.send("1st response!!");
-        next();
-        
-    },
+app.get("/admin/getdata",(req,res)=>{
+    console.log("admin got the data");
+    res.send("data has been send");
+})
 
-    (req,res,next)=>{
-        console.log("2nd route handler")
-        // res.send("1st response!!");
-        next();
-        
-    },
-)
+app.delete("/admin/delete",(req,res)=>{
+    console.log("admin wants to delete the data");
+    res.send("data has been deleted");
+})
 
 app.listen(7000,()=>{
     console.log("welcome to port no 7000!!")
