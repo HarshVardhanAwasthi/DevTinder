09161bee46a204c01a21bd9b5644e492bd93b52c
Harsh V Awasthi <evearywherefor4@gmail.com>
2025-01-21 20:28:57 +0530

    data send by user to database not hardcoding

--- src/app.js
+++ src/app.js
@@ -6,35 +6,16 @@ const User=require("./models/user.js");
 
 const connectDB=require("./config/database.js");
 
-
-//When a client (such as a frontend or API testing tool like Postman) sends a POST request to /signup, a new user document is created and stored in the MongoDB database using Mongoose.
-
-//Uses async function: Since saving data in MongoDB is an asynchronous operation, we use async/await.
-
+app.use(express.json());//It is used as middleware to parse incoming JSON data from requests.
+//Express does not parse request bodies. If a client sends JSON data in a POST, PUT, or PATCH request, you won’t be able to access it without express.json().
 app.post("/signup",async (req,res)=>{
-    /*
-        const user = new User({.......})
-        Creates a new User object:
-        -This initializes a new user using the Mongoose model (User).
-        -The User model ensures that the document follows the structure defined in the Mongoose schema.
     
-    */
-    const user= new User({
-        "firstName":"Harss",
-        "lastName":"Awassthi",
-        "gender":"male",
-        "emailId":"logo@1.com",
-        "age":23,
-        "password":"@123@hrs",
-    });
+    const user= new User(req.body);
+    //req.body is an object that contains data sent in the request body. It is used when a client (frontend, Postman, etc.) sends data to the server using POST, PUT, or PATCH requests.
 
-    /*
-        await user.save();
-        Saves the user in MongoDB:
-        -user.save() is an asynchronous operation that inserts the document into the MongoDB collection.
-        -await ensures that we wait for the database operation to complete before proceeding. */
+
     await user.save();
-    res.send("account created successfully");  // Once the user is saved successfully, the server responds with "account created successfully", letting the client know that the signup process was successful.
+    res.send("account created successfully"); 
 })
 
 connectDB().then(()=>{
